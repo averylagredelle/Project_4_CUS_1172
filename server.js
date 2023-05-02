89b115c960bc909a5aef28610a49c79c46e60044
averylagredelle <averylagredelle@gmail.com>
2023-05-02 12:36:57 -0400

    Revert "Minor changes to meet project requirements"
    This reverts commit 2b2f1c79653ed08443fa049a51e189bebf0c5ab1.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-const session = require("express-session");
-const auth_router = require("./auth_routes.js");
-const video_router = require("./video_routes.js");
+const router = require("./routes.js");
 
 let db_file = "./Resources/database.json";
 let db_format = {
@@ -12,14 +10,7 @@ let db_format = {
 
 global.db = require("./database.js")(db_file, db_format);
 
-app.use(session({
-    secret: "SecretCodeLoadedFromEnvironment",
-    saveUninitialized: true,
-    resave: false,
-    cookie: {maxAge: 300000}
-}));
-app.use("/auth", auth_router);
-app.use("/video", video_router);
+app.use("/app", router);
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000!")
